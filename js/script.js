412271c3f979ef46ac1d788b0fceb1d7e46966ba
JavaSquirt <c58.krypt0n@gmail.com>
2019-01-16 19:29:19 +0300

    js

--- js/script.js
+++ js/script.js
@@ -3,15 +3,32 @@
     var link = document.querySelector(".login-link");
     var popup = document.querySelector(".modal-login");
     var close = document.querySelector(".modal-close");
+    var form = popup.querySelector("form");
+    var login = popup.querySelector("[name=login]");
+    var password = popup.querySelector("[name=password]");
+    var storage = localStorage.getItem("login");
 
     link.addEventListener("click", function (evt) {
         evt.preventDefault();
         popup.classList.add("modal-show");
+        login.focus();
+        if (storage) {
+            login.value = storage;
+        }
     });
 
     close.addEventListener("click", function (evt) {
         evt.preventDefault();
         popup.classList.remove("modal-show");
+    });    
+
+    form.addEventListener("submit", function (evt) {
+        if (!login.value || !password.value) {
+            evt.preventDefault();
+            console.log("SAY FRIEND AND COME IN");            
+        } else {
+            localStorage.setItem("login", login.value);
+        }
     });
 
 </script>
