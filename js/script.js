8b34a319b51d9c6f3dec9e16ff06a35ee331b4e8
JavaSquirt <c58.krypt0n@gmail.com>
2019-01-17 14:14:57 +0300

    pravki

--- js/script.js
+++ js/script.js
@@ -1,33 +1,62 @@
 <script>
 
-    var link = document.querySelector(".login-link");
-    var popup = document.querySelector(".modal-login");
+    var link = document.querySelector(".btn-map");
+    var popup = document.querySelector(".modal-mail");
     var close = document.querySelector(".modal-close");
     var form = popup.querySelector("form");
     var login = popup.querySelector("[name=login]");
     var password = popup.querySelector("[name=password]");
-    var storage = localStorage.getItem("login");
+    
+    var = isStorageSupport = true;
+    var storage = "";
+
+    try {
+        storage = localStorage.getItem("login");
+    } catch (err) {
+        isStorageSupport = false;
+    }
 
     link.addEventListener("click", function (evt) {
         evt.preventDefault();
         popup.classList.add("modal-show");
-        login.focus();
+        
         if (storage) {
             login.value = storage;
+            password.focus();
+        } else {
+            login.focus();
         }
     });
 
     close.addEventListener("click", function (evt) {
         evt.preventDefault();
         popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
     });    
 
     form.addEventListener("submit", function (evt) {
         if (!login.value || !password.value) {
             evt.preventDefault();
-            console.log("SAY FRIEND AND COME IN");            
+            console.log("SAY FRIEND AND COME IN");
+            popup.classList.remove("modal-error");
+            popup.offsetWidth = popup.offsetWidth;
+            popup.classList.add("modal-error");        
         } else {
-            localStorage.setItem("login", login.value);
+            if (isStorageSupport) {
+                localStorage.setItem("login", login.value);
+            }            
+        }
+    });
+
+    // Закрыть попап нажатием ESC
+
+    document.addEventListener("keydown", function (evt) {
+        if (evt.keyCode === 27) {
+            if (popup.classList.contains("modal-show")) {
+                evt.preventDefault();
+                popup.classList.remove("modal-show");
+                popup.classList.remove("modal-error");
+            }
         }
     });
 
